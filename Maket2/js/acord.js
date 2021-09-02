692f8ca7bad18af8e116be2a37b4754a950c8d8f
momotYura13 <79109681+antimix13@users.noreply.github.com>
2021-09-02 14:28:00 +0300

    proect

---
+++ Maket2/js/acord.js
@@ -0,0 +1,21 @@
+function onToggle(event) {
+  if (event.target.open) {
+    document.querySelectorAll(".info_box1 > details[open]").forEach((el) => {
+      // Исключаем из перебора елемент который мы только что открыли
+      if (el === event.target) {
+        return;
+      }
+
+      // Закрываем все остальные елементы <details>
+      el.open = false;
+    });
+  }
+}
+
+// Вешаем наблюдатель на все елементы <details> внутри акордиона
+document
+  .querySelectorAll(".info_box1 > details")
+  .forEach((el) => el.addEventListener("toggle", onToggle));
+
+  
+
